5f4063fd909033de97e942669c46973df170f527
HumbertoCasares98 <64377520+HumbertoCasares98@users.noreply.github.com>
2021-08-24 00:16:20 -0500

    Primer commit

---
+++ boton.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  double val = 0;
+  String botonIF = "Iniciar", titulo="Iniciar";
+  Color colorBotonIF = Colors.green;
+  bool _isPaused;
+  
+   while (_isPaused) {
+        await Future.delayed(Duration(milliseconds: 500));
+      }
+      
+  void change() {
+    setState(() {
+      if(botonIF == "Iniciar"){
+        botonIF = "Detener";
+        titulo="Detener";
+        colorBotonIF = Colors.red;
+        val++;
+      }else{
+        botonIF = "Iniciar";
+        titulo="Iniciar";
+        colorBotonIF = Colors.green;
+        val=val+2;
+      }
+	
+	_isPaused? _isPaused=false: _isPaused=true;
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titulo),
+       backgroundColor: colorBotonIF
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(child: Text('$val'))),
+            ElevatedButton(
+              child: Text(botonIF),
+              onPressed: change,
+              style: ElevatedButton.styleFrom(
+                primary: colorBotonIF,
+                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                textStyle: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Counter(),
+      ),
+    );
+  }
+}
+
+Future<void> main() async {
+  runApp(MyApp());
+}
